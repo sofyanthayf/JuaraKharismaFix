3d77e3305de6d623900efe24d9e60787e9f304d6
Sofyan Thayf <sofyanthayf@users.noreply.github.com>
2019-07-23 21:02:54 +0800

    Update database.php
    using environment variables for database connection

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'ahyar',
-	'password' => 'ahyar',
-	'database' => 'kharisma',
+	'hostname' => getenv('DB_HOST'),
+	'username' => getenv('DB_USER'),
+	'password' => getenv('DB_PASS'),
+	'database' => getenv('DB_DATA'),
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
